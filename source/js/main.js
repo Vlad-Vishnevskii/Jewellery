937e1feef4fc8273e84874dda2075773b2839c1b
Vlad <vishnevskii_vlad@mail.ru>
2021-07-11 17:34:28 +0300

    Добавляет пп главной страницы десктопа

--- source/js/main.js
+++ source/js/main.js
@@ -70,36 +70,42 @@
 
 
 
-  // accordion
+  //accordion
 
-  // var footerTop = document.querySelector('.footer__top');
-  // var accordionsNoJS = footerTop.querySelectorAll('.accordion_no-js');
-  // var accordionContentNoJS = footerTop.querySelectorAll('.accordion__content_no-js');
-  // var accordions = document.querySelectorAll('.accordion');
 
-  // function deleteNoJS() {
-  //   for (var i = 0; i < accordionsNoJS.length; i++) {
-  //     accordionsNoJS[i].classList.remove('accordion_no-js');
-  //   }
-  //   for (var j = 0; j < accordionContentNoJS.length; j++) {
-  //     accordionContentNoJS[j].classList.remove('accordion__content_no-js');
-  //   }
-  // }
+  var accordionsNoJS = document.querySelectorAll('.accordion_no-js');
+  var accordionContentNoJS = document.querySelectorAll('.accordion__content_no-js');
+  var questionsItemNoJS = document.querySelectorAll('.questions__item_no-js');
+  var accordions = document.querySelectorAll('.accordion');
 
-  // deleteNoJS();
+  console.log(accordions)
 
-  // accordions.forEach(function (item) {
-  //   item.addEventListener('click', function () {
-  //     if (item.classList.contains('accordion_active')) {
-  //       item.classList.remove('accordion_active');
-  //     } else {
-  //       var activeAccordion = document.querySelector('.accordion_active');
-  //       if (activeAccordion && activeAccordion !== item) {
-  //         activeAccordion.classList.remove('accordion_active');
-  //       }
-  //       item.classList.add('accordion_active');
-  //     }
-  //   });
-  // });
+  function deleteNoJS() {
+    for (var i = 0; i < accordionsNoJS.length; i++) {
+      accordionsNoJS[i].classList.remove('accordion_no-js');
+    }
+    for (var j = 0; j < accordionContentNoJS.length; j++) {
+      accordionContentNoJS[j].classList.remove('accordion__content_no-js');
+    }
+    for (var k = 0; k < questionsItemNoJS.length; k++) {
+      questionsItemNoJS[k].classList.remove('questions__item_no-js');
+    }
+  }
+
+  deleteNoJS();
+
+  accordions.forEach(function (item) {
+    item.addEventListener('click', function () {
+      if (item.classList.contains('accordion_active')) {
+        item.classList.remove('accordion_active');
+      } else {
+        var activeAccordion = document.querySelector('.accordion_active');
+        if (activeAccordion && activeAccordion !== item) {
+          activeAccordion.classList.remove('accordion_active');
+        }
+        item.classList.add('accordion_active');
+      }
+    });
+  });
 
 })();
